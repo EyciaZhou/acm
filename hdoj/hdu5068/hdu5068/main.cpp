1aefc2c9284d6326a862433062c1d1578ca8b095
eycia zhou <zhou.eycia@gmail.com>
2015-05-10 14:24:52 +0800

    2015-05-10 14:24:52

--- hdoj/hdu5068/hdu5068/main.cpp
+++ hdoj/hdu5068/hdu5068/main.cpp
@@ -7,39 +7,65 @@
 //
 
 #include <iostream>
+#include <cstring>
 #define MOD 1000000007
 #define lc(a) (a*2)
 #define rc(a) (a*2+1)
 #define ll long long
 using namespace std;
 
-bool stat[50005][2][2];
-ll v[200005];
+struct mat{
+    ll a[2][2];
+    mat(ll a1, ll a2, ll b1, ll b2) {
+        a[0][0] = a1;
+        a[0][1] = a2;
+        a[1][0] = b1;
+        a[1][1] = b2;
+    }
+    mat() {
+        a[0][0] = 0;
+        a[0][1] = 0;
+        a[1][0] = 0;
+        a[1][1] = 0;
+    }
+};
+
+mat one = mat(1, 0, 0, 1);
+mat all = mat(1, 1, 1, 1);
+
+mat operator*(const mat &l, const mat &r) {
+    return mat((l.a[0][0]*r.a[0][0]+l.a[0][1]*r.a[1][0])%MOD, (l.a[0][0]*r.a[0][1]+l.a[0][1]*r.a[1][1])%MOD,
+               (l.a[1][0]*r.a[0][0]+l.a[1][1]*r.a[1][0])%MOD, (l.a[1][0]*r.a[0][1]+l.a[1][1]*r.a[1][1])%MOD);
+}
+
+mat stat[50005];
+mat v[200005];
 
 void build(int n, int l, int r) {
     if (l > r) return;
-    if (l == r) {v[n] = 2; return;}
+    if (l == r) {v[n] = all; return;}
     build(lc(n), l, (l+r)/2);
     build(rc(n), (l+r)/2+1, r);
-    //update
-    v[n] = (v[lc(n)] * v[rc(n)]) % MOD;
+    v[n] = v[lc(n)] * v[rc(n)];
 }
 
-void modify(int n, int l, int r, int lhs, int del) {
+void modify(int n, int l, int r, int lhs, int xx, int yy, int del) {
     if (l > r) return;
-    if (l == r) {v[n] += del; return;}
+    if (l == r) {v[n].a[xx][yy] += del; return;}
     if (lhs <= (l+r)/2) {
-        modify(lc(n), l, (l+r)/2, lhs, del);
+        modify(lc(n), l, (l+r)/2, lhs, xx, yy, del);
     } else {
-        modify(rc(n), (l+r)/2+1, r, lhs, del);
+        modify(rc(n), (l+r)/2+1, r, lhs, xx, yy, del);
     }
-    //update
-    v[n] = (v[lc(n)] * v[rc(n)]) % MOD;
+    v[n] = v[lc(n)] * v[rc(n)];
 }
 
-ll query(int n, int l, int r, int lhs, int rhs) {
-    cout << l << " " << r << " " << lhs << " " << rhs << endl;
-    if (l > r) return 1;
+void pmat(mat res) {
+    printf("%lld %lld %lld %lld\n", res.a[0][0], res.a[0][1], res.a[1][0], res.a[1][1]);
+}
+
+mat query(int n, int l, int r, int lhs, int rhs) {
+    if (l > r) return one;
     if (l == r) {return v[n];}
     if (l == lhs && r == rhs) {return v[n];}
     if (rhs <= (l+r) / 2) {
@@ -47,14 +73,9 @@ ll query(int n, int l, int r, int lhs, int rhs) {
     } else if (lhs > (l+r)/2) {
         return query(rc(n), (l+r)/2+1, r, lhs, rhs);
     } else {
-        return query(lc(n), l, (l+r)/2, lhs, (l+r)/2) *
-        query(rc(n), (l+r)/2+1, r, (l+r)/2+1, rhs);
+        return query(lc(n), l, (l+r)/2, lhs, (l+r)/2) * query(rc(n), (l+r)/2+1, r, (l+r)/2+1, rhs);
     }
 }
-
-/*
-4 3 2 1 0
-*/
 
 int main(int argc, const char * argv[]) {
     int n, m;
@@ -66,16 +87,23 @@ int main(int argc, const char * argv[]) {
         for (int i = 0; i < m; i++) {
             scanf("%d", &o);
             if (o == 0) {
+                ll tot = 0;
                 scanf("%d%d", &l, &r);
-                printf("%lld", query(1, 1, n-1, l, r-1));
+                mat res = query(1, 1, n-1, l, r-1);
+                for (int i = 0; i < 2; i++) {
+                    for (int j = 0; j < 2; j++) {
+                        tot = (tot + res.a[i][j]) % MOD;
+                    }
+                }
+                printf("%d\n", tot);
             } else {
                 scanf("%d%d%d", &x, &y, &z);
-                if (stat[x][y-1][z-1]) {
-                    stat[x][y-1][z-1] = false;
-                    modify(1, 1, n-1, x, -1);
+                if (stat[x].a[y-1][z-1]) {
+                    stat[x].a[y-1][z-1] = false;
+                    modify(1, 1, n-1, x, y-1, z-1, -1);
                 } else {
-                    stat[x][y-1][z-1] = true;
-                    modify(1, 1, n-1, x, 1);
+                    stat[x].a[y-1][z-1] = true;
+                    modify(1, 1, n-1, x, y-1, z-1, 1);
                 }
             }
         }
